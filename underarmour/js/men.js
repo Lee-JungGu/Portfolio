aea3dac776b70e7bd64920c9a8fc786217b39821
Lee-JungGu <whatmyname@naver.com>
2021-01-05 17:01:56 +0900

    멘 페이지 모바일 반응형 구조 변경, 화살표 추가 및 이벤트 추가

--- underarmour/js/men.js
+++ underarmour/js/men.js
@@ -16,4 +16,17 @@ $('.horizontal_slider').slick({
         }
     ]
   });
-  
+
+  function animateMobileGnb(){
+      $('.menu_box h2').click(function(){
+            var checkDisplay = $('.menu_box ul').css('display');
+            if(checkDisplay == "none"){
+                $('.menu_box ul').stop().show();
+                $('.menu_box h2 img').css("transform","rotate(180deg)");
+            }else if(checkDisplay == "block"){
+                $('.menu_box ul').stop().hide();
+                $('.menu_box h2 img').css("transform","rotate(0deg)");
+            }
+        });
+  }
+  animateMobileGnb();
