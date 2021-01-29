7efec31d1e0e89fb049738a8c52cd4a67be49cfc
Lee-JungGu <whatmyname@naver.com>
2021-01-29 14:40:21 +0900

    비교 판정 영역 객체화 작업

--- baseballgame/js/baseball.js
+++ baseballgame/js/baseball.js
@@ -4,67 +4,68 @@ function randomStrikeNumber(){
     let rangeNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     for(let i = 0; i < 3; i++){
         let randomNum = Math.floor(Math.random() * (10 - i));
-        strikeNumber[i] = rangeNumber[randomNum]
-        rangeNumber.splice(randomNum, 1) ;
+        strikeNumber[i] = rangeNumber[randomNum];
+        rangeNumber.splice(randomNum, 1);
     }
 }
 
 function drawInputBox(){
     document.getElementById('content').innerHTML =
-    '<input id="textbox" type="textbox" onchange="drawJudgment()">'
+    '<input id="textbox" type="textbox" onchange="judgement.drawJudgement()">'
     + '<button>확인</button>'
-    + '<p id="result1"></p>'
-    + '<p id="result2"></p>'
-    + '<p id="result3"></p>';
+    + '<p id="strike"></p>'
+    + '<p id="ball"></p>'
+    + '<p id="out"></p>';
 }
 
-function judgeNumber(){
-    let findInput = document.getElementById('textbox').value;
-    let firstNumber = Math.floor(findInput / 100);
-    let secondNumber = Math.floor((findInput % 100) / 10);
-    let thirdNumber = (findInput % 100) % 10;
-    let strike = 0;
-    let ball = 0;
-    let out = 0;
-
-    function compareNumber(numericOrder, indexNum1, indexNum2, indexNum3){
+let judgement = {
+    
+    strike: 0,
+    ball: 0,
+    out: 0,
+    
+    compareNumber: function(numericOrder, indexNum1, indexNum2, indexNum3){
         if(numericOrder == strikeNumber[indexNum1]){
-            strike++;
+            this.strike++;
         }else if(numericOrder == strikeNumber[indexNum2]){
-            ball++;
+            this.ball++;
         }else if(numericOrder == strikeNumber[indexNum3]){
-            ball++;
+            this.ball++;
         }else{
-            out++;
+            this.out++;
         }
-    }
+    },
 
-    function drawNumber(){
-        document.getElementById('result1').innerHTML = strike + ' Strike';
-        document.getElementById('result2').innerHTML = ball + ' Ball';
-        document.getElementById('result3').innerHTML = out + ' Out';
-    }
-    
-    compareNumber(firstNumber, 0, 1, 2);
-    compareNumber(secondNumber, 1, 0, 2);
-    compareNumber(thirdNumber, 2, 0, 1);
+    drawNumber: function(){
+        document.getElementById('strike').innerHTML = this.strike + ' Strike';
+        document.getElementById('ball').innerHTML = this.ball + ' Ball';
+        document.getElementById('out').innerHTML = this.out + ' Out';
+    },
 
-    drawNumber();
-    
-    return strike;
+    sucessGame: function(){
+        if(judgement.strike == 3){
+            alert('게임 성공!!!!!')
+        }
+    },
+
+    drawJudgement: function(){
+        let findInput = document.getElementById('textbox').value;
+        let firstNumber = Math.floor(findInput / 100);
+        let secondNumber = Math.floor((findInput % 100) / 10);
+        let thirdNumber = (findInput % 100) % 10;
+
+        this.compareNumber(firstNumber, 0, 1, 2);
+        this.compareNumber(secondNumber, 1, 0, 2);
+        this.compareNumber(thirdNumber, 2, 0, 1);
+        this.drawNumber();
+        this.sucessGame();
+        
+        this.strike = 0;
+        this.ball = 0;
+        this.out = 0;
+    },
+
 }
 
-function drawJudgment(){
-    judgeNumber();
-    sucessGame();
-}
-
-function sucessGame(){
-    let strike = judgeNumber();
-    if(strike == 3){
-        alert('게임 성공!!!!!')
-    }
-}
-
-randomStrikeNumber();
 drawInputBox();
+randomStrikeNumber();
