ee0bed899193b317b6159ab3560bf31d0babb122
Lee-JungGu <whatmyname@naver.com>
2021-02-01 18:21:30 +0900

    시작 이미지 추가 및 전광판 디자인, 코드 정리

--- baseballgame/js/baseball.js
+++ baseballgame/js/baseball.js
@@ -1,68 +1,40 @@
 let strikeNumber = [];
 
-function randomStrikeNumber(){
-    let rangeNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    for(let i = 0; i < 3; i++){
-        let randomNum = Math.floor(Math.random() * (10 - i));
-        strikeNumber[i] = rangeNumber[randomNum];
-        rangeNumber.splice(randomNum, 1);
-    }
-    console.log(strikeNumber);
-}
-
-function drawInputBox(){
-    document.getElementById('content').innerHTML =
-    '<input id="textbox" type="textbox" maxlength="3" onchange="judgement.drawJudgement()">'
-    + '<div id="judgeBox"><p id="strike"></p>'
-    + '<p id="ball"></p>'
-    + '<p id="out"></p>'
-    + '<p id="record"></p></div>'
-    + '<div id="keypad"><div><button data-num="1">1</button><img src="images/baseball.png" alt="야구공이미지"></div>'
-    + '<div><button data-num="2">2</button><img src="images/baseball.png" alt="야구공이미지"></div>'
-    + '<div><button data-num="3">3</button><img src="images/baseball.png" alt="야구공이미지"></div>'
-    + '<div><button data-num="4">4</button><img src="images/baseball.png" alt="야구공이미지"></div>'
-    + '<div><button data-num="5">5</button><img src="images/baseball.png" alt="야구공이미지"></div>'
-    + '<div><button data-num="6">6</button><img src="images/baseball.png" alt="야구공이미지"></div>'
-    + '<div><button data-num="7">7</button><img src="images/baseball.png" alt="야구공이미지"></div>'
-    + '<div><button data-num="8">8</button><img src="images/baseball.png" alt="야구공이미지"></div>'
-    + '<div><button data-num="9">9</button><img src="images/baseball.png" alt="야구공이미지"></div>'
-    + '<div><button id="del" data-num="">Del</button></div>'
-    + '<div><button data-num="0">0</button><img src="images/baseball.png" alt="야구공이미지"></div>'
-    + '<div><button id="throw" data-num="">Throw</button></div>'
-    + '</div>';
-}
-
-function clickKeypad(){
-    let keyButtons = document.querySelectorAll('#keypad div button');
-    keyButtons.forEach(function(keyButton){
-        keyButton.addEventListener('click', function(){
-            let throwBall = document.getElementById('throw');
-            let delNumber = document.getElementById('del');
-            let checkData = this.dataset.num;
-            if(this == throwBall){
-                judgement.drawJudgement();
-                document.querySelectorAll('.flip_ball').forEach(function(flipBall){
-                    flipBall.setAttribute('class', '');
-                });
-                document.getElementById('record').innerHTML += document.getElementById('textbox').value + '<br>';
-                document.getElementById('textbox').value = '';
-            }else if(this == delNumber){
-                document.getElementById('textbox').value = "";
-                document.querySelectorAll('.flip_ball').forEach(function(flipBall){
-                    flipBall.setAttribute('class', '');
-                });
-            }
-            if(document.getElementById('textbox').value.length < 3){
-                document.getElementById('textbox').value += this.dataset.num;
-            }
-            if(!checkData == ""){
-                if(document.getElementsByClassName('flip_ball').length < 6){
-                    this.setAttribute('class', 'flip_ball');
-                    this.nextSibling.setAttribute('class', 'flip_ball');
-                }
-            }
-        });
-    });
+let prepareGame = {
+    randomStrikeNumber: function(){
+        let rangeNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for(let i = 0; i < 3; i++){
+            let randomNum = Math.floor(Math.random() * (10 - i));
+            strikeNumber[i] = rangeNumber[randomNum];
+            rangeNumber.splice(randomNum, 1);
+        }
+    },
+    hideStartImage: function(){
+        setTimeout(function(){
+            document.getElementById('start_img').style.display = 'none';
+        }, 1600)
+    },    
+    drawInputBox: function(){
+        document.getElementById('content').innerHTML =
+        '<input id="textbox" type="textbox" maxlength="3" onchange="judgement.drawJudgement()">'
+        + '<div id="judgeBox"><p id="strike"></p>'
+        + '<p id="ball">&nbsp;Ready</p>'
+        + '<p id="out"></p>'
+        + '<p id="record"></p></div>'
+        + '<div id="keypad"><div><button data-num="1">1</button><img src="images/baseball.png" alt="야구공이미지"></div>'
+        + '<div><button data-num="2">2</button><img src="images/baseball.png" alt="야구공이미지"></div>'
+        + '<div><button data-num="3">3</button><img src="images/baseball.png" alt="야구공이미지"></div>'
+        + '<div><button data-num="4">4</button><img src="images/baseball.png" alt="야구공이미지"></div>'
+        + '<div><button data-num="5">5</button><img src="images/baseball.png" alt="야구공이미지"></div>'
+        + '<div><button data-num="6">6</button><img src="images/baseball.png" alt="야구공이미지"></div>'
+        + '<div><button data-num="7">7</button><img src="images/baseball.png" alt="야구공이미지"></div>'
+        + '<div><button data-num="8">8</button><img src="images/baseball.png" alt="야구공이미지"></div>'
+        + '<div><button data-num="9">9</button><img src="images/baseball.png" alt="야구공이미지"></div>'
+        + '<div><button id="del" data-num="">Del</button></div>'
+        + '<div><button data-num="0">0</button><img src="images/baseball.png" alt="야구공이미지"></div>'
+        + '<div><button id="throw" data-num="">Throw</button></div>'
+        + '</div>';
+    },
 }
 
 let judgement = {
@@ -70,6 +42,7 @@ let judgement = {
     strike: 0,
     ball: 0,
     out: 0,
+    numberOfAttempts: 0,
     
     compareNumber: function(numericOrder, indexNum1, indexNum2, indexNum3){
         if(numericOrder == strikeNumber[indexNum1]){
@@ -84,45 +57,63 @@ let judgement = {
     },
     
     drawNumber: function(){
+        this.numberOfAttempts++;
+
         document.getElementById('strike').innerHTML = this.strike + ' Strike';
         document.getElementById('ball').innerHTML = this.ball + ' Ball';
         document.getElementById('out').innerHTML = this.out + ' Out';
+        document.getElementById('record').innerHTML += this.numberOfAttempts + '.&nbsp;' + document.getElementById('textbox').value + '<br>';
+        document.getElementById('textbox').value = '';
+
     },
-    
-    playAnimation: function(){
-        if(this.out == 3){
-            document.getElementById('threeOut').style.display = 'block';
-            document.getElementById('threeOut').play();
-            document.getElementById('threeOut').addEventListener('ended', function(){
-                document.getElementById('threeOut').style.display = 'none';
+
+    clickKeypad: function(){
+        let keyButtons = document.querySelectorAll('#keypad div button');
+        keyButtons.forEach(function(keyButton){
+            keyButton.addEventListener('click', function(){
+                let throwBall = document.getElementById('throw');
+                let delNumber = document.getElementById('del');
+                let checkData = this.dataset.num;
+                if(this == throwBall){
+                    judgement.drawJudgement();
+                    document.querySelectorAll('.flip_ball').forEach(function(flipBall){
+                        flipBall.setAttribute('class', '');
+                    });
+                }else if(this == delNumber){
+                    document.getElementById('textbox').value = "";
+                    document.querySelectorAll('.flip_ball').forEach(function(flipBall){
+                        flipBall.setAttribute('class', '');
+                    });
+                }
+                if(document.getElementById('textbox').value.length < 3){
+                    document.getElementById('textbox').value += this.dataset.num;
+                }
+                if(!checkData == ""){
+                    if(document.getElementsByClassName('flip_ball').length < 6){
+                        this.setAttribute('class', 'flip_ball');
+                        this.nextSibling.setAttribute('class', 'flip_ball');
+                    }
+                }
             });
-        }else if(this.out == 2){
-            document.getElementById('twoOut').style.display = 'block';
-            document.getElementById('twoOut').play();
-            document.getElementById('twoOut').addEventListener('ended', function(){
-                document.getElementById('twoOut').style.display = 'none';
-            });
-        }else if(this.out == 1){
-            document.getElementById('oneOut').style.display = 'block';
-            document.getElementById('oneOut').play();
-            document.getElementById('oneOut').addEventListener('ended', function(){
-                document.getElementById('oneOut').style.display = 'none';
-            });
-        }else if(this.out == 0 && this.ball != 0){
-            document.getElementById('oneOut').style.display = 'block';
-            document.getElementById('oneOut').play();
-            document.getElementById('oneOut').addEventListener('ended', function(){
-                document.getElementById('oneOut').style.display = 'none';
-            });
-        }
+        });
     },
 
     sucessGame: function(){
         if(judgement.strike == 3){
-            // alert('게임 성공!!!!!')
+            document.querySelector('#animation img').style.display = 'none';
             document.getElementById('succes').style.display = 'block';
             document.getElementById('succes').play();
-            document.getElementById('record').innerHTML = '';
+            document.getElementById('record').innerHTML = '횟수:' + this.numberOfAttempts;
+            this.numberOfAttempts = 0;
+            setTimeout(function(){
+                prepareGame.randomStrikeNumber();
+                document.getElementById('strike').innerHTML = "";
+                document.getElementById('ball').innerHTML = "&nbsp;Ready";
+                document.getElementById('out').innerHTML = "";
+                document.getElementById('record').innerHTML = "";
+                document.getElementById('succes').style.display = 'none';
+                document.querySelector('#animation img').style.display = 'block';
+            }, 9400);
         }
     },
     
@@ -136,9 +127,8 @@ let judgement = {
         this.compareNumber(secondNumber, 1, 0, 2);
         this.compareNumber(thirdNumber, 2, 0, 1);
         this.drawNumber();
-        this.playAnimation();
         this.sucessGame();
-        
+
         this.strike = 0;
         this.ball = 0;
         this.out = 0;
@@ -146,7 +136,8 @@ let judgement = {
     
 }
 
-randomStrikeNumber();
-drawInputBox();
-clickKeypad();
+prepareGame.randomStrikeNumber();
+prepareGame.hideStartImage();
+prepareGame.drawInputBox();
+judgement.clickKeypad();
 
